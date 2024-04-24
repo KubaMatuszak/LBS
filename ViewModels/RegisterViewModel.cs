d0d6f0cea09402b58c3810f1c1238ece84504d79
KubaMatuszak <kuba.matuszak80@gmail.com>
2024-04-24 12:59:35 +0200

    Dodano widok rejestracji

--- ViewModels/RegisterViewModel.cs
+++ ViewModels/RegisterViewModel.cs
@@ -4,17 +4,18 @@ namespace LBS.ViewModels
 {
 	public class RegisterViewModel
 	{
-        [Required]
+        [Required(ErrorMessage ="To pole jest wymagane")]
         public string FirstName { get; set; }
-        [Required]
-        public string LastName { get; set; }
-        [Required]
-        [DataType(DataType.EmailAddress)]
+		[Required(ErrorMessage = "To pole jest wymagane")]
+		public string LastName { get; set; }
+		[Required(ErrorMessage = "To pole jest wymagane")]
+		[DataType(DataType.EmailAddress)]
         public string Email { get; set; }
-        [Required]
-        [DataType(DataType.Password)]   
+		[Required(ErrorMessage = "To pole jest wymagane")]
+		[DataType(DataType.Password)]   
         public string Password { get; set; }
         [Compare("Password", ErrorMessage = "Hasła nie są takie same")]
+        [Display(Name ="Potwierdź hasło")]
         public string ConfirmPassword { get; set; }
 
     }
