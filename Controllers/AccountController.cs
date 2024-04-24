553f951f056b3aa455b60c33765399bf9199cd3f
KubaMatuszak <kuba.matuszak80@gmail.com>
2024-04-24 12:47:03 +0200

    Dodane w pełnie funkcjonalne logowanie

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LBS.Models;
+using LBS.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LBS.Controllers
 {
 	public class AccountController : Controller
 	{
-		public IActionResult Login()
+		private readonly SignInManager<LBSUser> _signInManager;
+        public AccountController(SignInManager<LBSUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+        public IActionResult Login()
 		{
 			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> Login(LoginViewModel model)
+		{
+			if(ModelState.IsValid) 
+			{
+				var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password,model.RememberMe,false);
+				if(result.Succeeded) 
+				{
+				return RedirectToAction("Index", "Home");
+				}
+				ModelState.AddModelError("", "Nieudana próba logowania");
+				return View(model);
+			}
+			
+			return View(model);
 		}
 		public IActionResult Register()
 		{
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using LBS.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace LBS.Controllers
 {
+	[Authorize]
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
