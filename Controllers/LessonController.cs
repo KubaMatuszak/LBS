c2dd916a9aefcbbba1c45b4be073748db974d268
KubaMatuszak <kuba.matuszak80@gmail.com>
2024-04-27 15:08:39 +0200

    Dodano widok i kontroler do dodawania pojedynczych zajęć przez usera (Dodać walidacje)

---
+++ Controllers/LessonController.cs
@@ -0,0 +1,49 @@
+﻿using LBS.Data;
+using LBS.Models;
+using LBS.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace LBS.Controllers
+{
+	[Authorize]
+	public class LessonController : Controller
+	{
+		private readonly LessonDbContext _context;
+		private readonly UserManager<LBSUser> _userManager;
+		public LessonController(LessonDbContext context, UserManager<LBSUser> user)
+		{
+			_context = context;
+			_userManager = user;
+		}
+
+		
+		public IActionResult Add()
+		{
+			
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> Add(LessonViewModel vmodel) 
+		{
+			string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+			LBSUser loggeduser = await _userManager.FindByIdAsync(userId);
+
+			Lesson lesson = new()
+			{
+				LessonDate = vmodel.LessonDate,
+				LessonLength = vmodel.LessonLength,
+				LessonTime = vmodel.LessonTime,
+				StudentFirstName = loggeduser.FirstName,
+				StudentLastName = loggeduser.LastName
+
+			};
+			_context.Add(lesson);
+			_context.SaveChanges();
+
+			return View();
+		}
+	}
+}
---
+++ ViewModels/LessonViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace LBS.ViewModels
+{
+	public class LessonViewModel
+	{
+		
+		public string StudentFirstName { get; set; }
+		public string StudentLastName { get; set; }
+		[Required(ErrorMessage ="Pole jest wymagane")]
+		[DataType(DataType.Date)]
+		[Display(Name ="Data zajęć")]
+		
+		public DateOnly LessonDate { get; set; }
+		[Required(ErrorMessage = "Pole jest wymagane")]
+		[DataType(DataType.Time)]
+		[Display(Name ="Godzina zajęć")]
+		public TimeOnly LessonTime { get; set; }
+		[Required(ErrorMessage = "Pole jest wymagane")]
+		[Display(Name = "Czas trwania zajęć w minutach")]
+
+		public int LessonLength { get; set; }
+	}
+}
