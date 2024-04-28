385e4ae16f2b7aa9bbca6c85dd5eb487463fc882
KubaMatuszak <kuba.matuszak80@gmail.com>
2024-04-28 13:13:39 +0200

    Dodana statyczna klasa przechowywująca informacje o dostępności nauczyciela

---
+++ Services/Awaliable.cs
@@ -0,0 +1,9 @@
+﻿namespace LBS.Services
+{
+	public static class Awaliable
+	{
+		public static TimeOnly MinHour { get; set; }
+		public static TimeOnly MaxHour { get; set;}
+		public static DayOfWeek[] PossibleDays { get; set;}
+	}
+}
